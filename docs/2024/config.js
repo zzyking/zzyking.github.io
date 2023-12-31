2e92df7b9ec9fce0cc8abba96237e001428f0f2e
臧泽元 <zangzeyuan@bupt.edu.cn>
2023-12-31 17:49:59 +0800

    2024 New Year

---
+++ docs/2024/config.js
@@ -0,0 +1,32 @@
+var design = anime({
+    targets: '#newyear2024 #happy',
+    strokeDashoffset: [setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 2000,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+});
+
+var design = anime({
+    targets: '#newyear2024 #NEWYEAR',
+    strokeDashoffset: [setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 2500,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+});
+
+
+
+var design = anime({
+    targets: '#newyear2024 #Vector_43,#Vector_210,#Vector_207,#Vector_42,#Vector_45',
+    translateY: -10,
+    easing: 'easeInOutSine',
+    duration: 2500,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+});
+
