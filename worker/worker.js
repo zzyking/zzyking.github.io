27753b6eadee897d8c98f2c23cac4444e34c2a5f
zzyking <zangzeyuan@bupt.edu.cn>
2026-01-16 11:24:39 +0800

    add cf worker

---
+++ worker/worker.js
@@ -0,0 +1,126 @@
+const ALLOWED_ORIGINS = new Set([
+  "https://zzyking.github.io",
+  "http://localhost:8787"
+]);
+const TTL_SECONDS = 60 * 60 * 24 * 30;
+
+function buildCorsHeaders(origin) {
+  var headers = {
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Max-Age": "86400"
+  };
+
+  if (origin && ALLOWED_ORIGINS.has(origin)) {
+    headers["Access-Control-Allow-Origin"] = origin;
+    headers["Vary"] = "Origin";
+  }
+
+  return headers;
+}
+
+function isAllowedRequest(origin, referer) {
+  if (origin && ALLOWED_ORIGINS.has(origin)) {
+    return true;
+  }
+
+  if (!referer) {
+    return false;
+  }
+
+  try {
+    var refUrl = new URL(referer);
+    return ALLOWED_ORIGINS.has(refUrl.origin);
+  } catch (err) {
+    return false;
+  }
+}
+
+async function parseBody(request) {
+  if (request.method === "GET") {
+    return Object.fromEntries(new URL(request.url).searchParams);
+  }
+
+  if (request.method !== "POST") {
+    return {};
+  }
+
+  var contentType = request.headers.get("content-type") || "";
+  if (contentType.indexOf("application/json") !== -1) {
+    try {
+      return await request.json();
+    } catch (err) {
+      return {};
+    }
+  }
+
+  var text = await request.text();
+  if (!text) {
+    return {};
+  }
+
+  try {
+    return JSON.parse(text);
+  } catch (err) {
+    return Object.fromEntries(new URLSearchParams(text));
+  }
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    var url = new URL(request.url);
+    if (url.pathname !== "/collect") {
+      return new Response("Not Found", { status: 404 });
+    }
+
+    var origin = request.headers.get("origin");
+    var corsHeaders = buildCorsHeaders(origin);
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, { status: 204, headers: corsHeaders });
+    }
+
+    if (request.method !== "GET" && request.method !== "POST") {
+      return new Response("Method Not Allowed", { status: 405, headers: corsHeaders });
+    }
+
+    var referer = request.headers.get("referer");
+    if (!isAllowedRequest(origin, referer)) {
+      return new Response("Forbidden", { status: 403, headers: corsHeaders });
+    }
+
+    if (!env.VISITS) {
+      return new Response("Missing KV binding", { status: 500, headers: corsHeaders });
+    }
+
+    var data = await parseBody(request);
+    var now = new Date().toISOString();
+    var safeTime = now.replace(/[:.]/g, "-");
+    var id = crypto.randomUUID();
+    var key = safeTime + "_" + id;
+
+    var cf = request.cf || {};
+    var record = {
+      id: id,
+      ts: now,
+      ip: request.headers.get("cf-connecting-ip"),
+      path: data.path || null,
+      ref: data.ref || null,
+      ua: request.headers.get("user-agent"),
+      country: cf.country || null,
+      region: cf.region || null,
+      city: cf.city || null,
+      postalCode: cf.postalCode || null,
+      latitude: cf.latitude || null,
+      longitude: cf.longitude || null,
+      timezone: cf.timezone || null,
+      continent: cf.continent || null,
+      colo: cf.colo || null,
+      asOrganization: cf.asOrganization || null
+    };
+
+    ctx.waitUntil(env.VISITS.put(key, JSON.stringify(record), { expirationTtl: TTL_SECONDS }));
+
+    return new Response(null, { status: 204, headers: corsHeaders });
+  }
+};
